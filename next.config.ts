d87e8266d728fd404ad471564e0fcb3e6c993228
deshanbyte <deshaaanw@gmail.com>
2026-03-28 16:16:06 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
-    // This is the magic line for Next.js 16
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This tells it to skip the linting check
     ignoreDuringBuilds: true,
   },
 };
